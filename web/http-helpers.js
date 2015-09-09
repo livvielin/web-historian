751d92cb986d0eb95e5fde83ed0f7fd3dc1bc52e
Hack Reactor Students <accounts@hackreactor.com>
2015-09-09 12:30:26 -0700

    Served up index.html and hacky post included

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,9 +11,14 @@ exports.headers = headers = {
 };
 
 exports.serveAssets = function(res, asset, callback) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...),
-  // css, or anything that doesn't change often.)
+  fs.readFile(path.join(archive.paths.siteAssets, asset), function(err, data){
+    if(err){
+      throw err;
+    }
+    res.end(data);
+  });  
+
+
 };
 
   // Write some code here that helps serve up your static files!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,44 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var body = ""
+  //req.on('data', handlePostChunk(chunk, body));
+  if(req.method === 'POST'){
+    req.on('data', function (chunk) {
+      body += chunk;
+    });
+    req.on('end', function() {
+      fs.appendFile(archive.paths.list, '/n' + body, function(err){
+        if(err){
+          throw err;
+        }
+        console.log('writing'+ body)
+        res.end();  
+      });
+    });
+  }
+  if ( req.url === '/' ) {
+    req.url = '/index.html';
+  }
+  helpers.serveAssets(res, req.url);
+};
+
+var handlePostChunk = function(chunk, body){
+  body += chunk;
+};
+
+var handlePostFinish = function(body){
+  fs.appendFile(archive.paths.list, '/n' + body, function(err){
+    if(err){
+      throw err;
+    }
+    console.log('writing'+ body)
+    res.end();  
+  });
+  
 };
