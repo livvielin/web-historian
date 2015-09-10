57890e4ea096f1b014634dc90334e64136c426c1
Hack Reactor Students <accounts@hackreactor.com>
2015-09-09 20:11:01 -0700

    Implemented workers

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
-// Use the code in `archive-helpers.js` to actually download the urls
-// that are waiting.
+var archive = require('../helpers/archive-helpers');
+var _ = require('underscore');
+
+archive.readListOfUrls(function(urls){
+  _.each(urls, function(url){
+    if(url !== '') {
+      archive.isUrlArchived(url, function(found){
+        if(!found){
+          archive.downloadUrls([url]);
+        }
+      });
+    }
+  });
+});
